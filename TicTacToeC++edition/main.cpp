76e08d746d33a57088432fe1cfccc0644425c7e8
Anthony <anth19800@hotmail.com>
2020-04-14 18:17:31 -0400

    [Add] Init

---
+++ TicTacToeC++edition/main.cpp
@@ -0,0 +1,24 @@
+
+int main() {
+data:
+	int grid[9] = { 0,0,0,0,0,0,0,0,0 };
+
+gameLoop:
+
+	//if player1 or player2 wins OR if turn == 9, exit
+
+	//determine whos turn it is
+
+	//draw the grid
+
+	//let them pick a spot by typing 0 (enter) 3 like a 2D array
+	//don't use an actual 2D array, let evyn do his dimension math
+
+	//draw the grid again
+
+	//determine if player1 or player2 has won
+
+	//goto gameLoop
+
+	return 0;
+}
